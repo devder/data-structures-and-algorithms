23e05b99f98c8fd1ade9d72dfed046a70292860e
hal <72111003+devder@users.noreply.github.com>
2025-02-24 22:36:29 +0100

    implement radix sort

--- 4.crazier-sorts/3.radix-sort.ts
+++ 4.crazier-sorts/3.radix-sort.ts
@@ -1,4 +1,6 @@
 /**
+ * time complexity = O(nk) - best, avg, worst
+ * space complexity = O(n+k)
  * other sorting algorithms work by doing comparisons but radix sort is different. It only works on lists of numbers.
  * It never makes comparisons between elements but exploits the fact that information information about the size of a number is encoded in the number of digits
  * It arranges numbers based on their lengths - 1, 22, 333, 4444... n
@@ -20,10 +22,32 @@ const digitCount = (num: number) => {
 
 // helper method mostDigits, returns the number of digits in the largest number in the list
 
-const mostDigits = (arr: number[]) => {
+const mostDigits = (nums: number[]) => {
   let val = 1;
-  for (let i = 0; i < arr.length; i++) {
-    val = Math.max(val, digitCount(arr[i]));
+  for (let i = 0; i < nums.length; i++) {
+    val = Math.max(val, digitCount(nums[i]));
   }
   return val;
 };
+
+/**
+ * Pseudocode for radix sort
+ * figure out how many digits the largest number has, loop from k=0 up to thus largest number of digits, for each iteration of the loop
+ * create buckets for each digit (0-9), place each number in the corresponding bucket based on its kth digit
+ */
+const radixSort = (nums: number[]) => {
+  let maxDigitCount = mostDigits(nums);
+
+  for (let i = 0; i < maxDigitCount; i++) {
+    const digitBuckets: number[][] = Array.from({ length: 10 }, () => []);
+
+    for (let j = 0; j < nums.length; j++) {
+      const dig = getDigit(nums[j], i);
+      digitBuckets[dig].push(nums[j]);
+    }
+    nums = ([] as number[]).concat(...digitBuckets);
+  }
+  return nums;
+};
+
+console.log(radixSort([23, 345, 5467, 12, 2345, 9852]));
