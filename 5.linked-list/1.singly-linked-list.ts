ddd0d88d0b7b192813de4f29631fbf1b535de767
hal <72111003+devder@users.noreply.github.com>
2025-03-08 13:25:29 +0100

    add insert/remove

--- 5.linked-list/1.singly-linked-list.ts
+++ 5.linked-list/1.singly-linked-list.ts
@@ -9,12 +9,13 @@
  * - 12(head) -> 15 -> 16 -> 25(tail)
  */
 
-const stringify = (val: any) => {
+const stringify = (val) => {
   console.log(JSON.stringify(val, null, 2));
 };
-const log = (val: any) => {
+const log = (val) => {
   console.log(val);
 };
+
 class Node {
   next: Node | null = null;
   constructor(public val: any) {}
@@ -33,7 +34,7 @@ class SinglyLinkedList {
       this.head = newNode;
       this.tail = newNode;
     } else {
-      this.tail!.next = newNode;
+      this.tail.next = newNode;
       this.tail = newNode;
     }
     this.length++;
@@ -42,7 +43,7 @@ class SinglyLinkedList {
 
   // remove the last node
   pop() {
-    if (!this.head) return undefined;
+    if (!this.head) return null;
     let current = this.head;
     let newTail = current;
 
@@ -63,7 +64,7 @@ class SinglyLinkedList {
 
   // remove the first node
   shift() {
-    if (!this.head) return undefined;
+    if (!this.head) return null;
     const current = this.head;
     if (!current.next) {
       this.tail = null;
@@ -92,11 +93,11 @@ class SinglyLinkedList {
 
   // get a node at an index
   get(index: number) {
-    if (index < 0 || index >= this.length) return undefined;
+    if (index < 0 || index >= this.length) return null;
 
     let current = this.head;
     for (let i = 1; i <= index; i++) {
-      current = current?.next!;
+      current = current.next;
     }
     return current;
   }
@@ -110,6 +111,32 @@ class SinglyLinkedList {
     }
     return false;
   }
+
+  // put a node at a specific index
+  insert(index: number, val: any) {
+    if (index < 0 || index > this.length) return null;
+    if (index === 0) return this.unShift(val);
+    if (index === this.length) return this.push(val);
+
+    const newNode = new Node(val);
+    const nodeAtIndexBefore = this.get(index - 1);
+    newNode.next = nodeAtIndexBefore.next;
+    nodeAtIndexBefore.next = newNode;
+    this.length++;
+    return this;
+  }
+
+  // remove an item from an index
+  remove(index: any) {
+    if (index < 0 || index >= this.length) return false;
+    if (index == 0) return !!this.shift();
+    if (index == this.length - 1) return !!this.pop();
+
+    const prev = this.get(index - 1);
+    prev.next = prev.next.next;
+    this.length--;
+    return true;
+  }
 }
 
 const l1 = new SinglyLinkedList();
@@ -120,5 +147,8 @@ l1.push("three");
 // log(l1.unShift("one"));
 // log(l1.get(0));
 // log(l1.set(2, 12));
-l1.set(1, "zero");
-log(l1.get(1));
+// l1.set(1, "zero");
+// log(l1.get(1));
+// stringify(l1.insert(3, "bool"));
+log(l1.remove(1));
+log(l1);
