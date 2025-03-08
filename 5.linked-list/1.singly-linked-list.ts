d4871acb8a6a599caeec784ecc78008d91566c6e
hal <72111003+devder@users.noreply.github.com>
2025-03-08 11:58:42 +0100

    add push/pop

--- 5.linked-list/1.singly-linked-list.ts
+++ 5.linked-list/1.singly-linked-list.ts
@@ -19,4 +19,43 @@ class SinglyLinkedList {
   tail: Node | null = null;
   length: number = 0;
   constructor() {}
+
+  push(val: any) {
+    const newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail!.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if (!this.head) return undefined;
+    let current = this.head;
+    let newTail = current;
+
+    while (current.next) {
+      newTail = current;
+      current = current.next;
+    }
+
+    newTail.next = null;
+    this.tail = newTail;
+    this.length--;
+    if (this.length === 0) {
+      this.head = null;
+      this.tail = null;
+    }
+    return current;
+  }
 }
+
+const l1 = new SinglyLinkedList();
+l1.push("one");
+l1.push("two");
+l1.push("three");
+console.log(l1.pop());
